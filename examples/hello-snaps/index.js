2c8f9172335711eea667acc0c8b1552bad35593c
zephyranthes03 <zephyranthes03@gmail.com>
2020-02-14 19:13:12 -0700

    Update index.js

--- examples/hello-snaps/index.js
+++ examples/hello-snaps/index.js
@@ -1,10 +1,18 @@
 wallet.registerRpcMessageHandler(async (originString, requestObject) => {
+  console.log("requestObject",requestObject);
   switch (requestObject.method) {
     case 'hello':
       return wallet.send({
         method: 'alert',
-        params: [`Hello, ${originString}!`]
+        params: [`Hello!, ${requestObject.method} =  ${originString}!`]
       })
+      break;
+    case 'bye':
+      return wallet.send({
+        method: 'alert',
+        params: [`Bye!!, ${originString}!`]
+      })
+      break;
     default:
       throw new Error('Method not found.')
   }
